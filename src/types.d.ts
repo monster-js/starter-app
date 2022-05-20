fb35bc42188048fa215cdcc2584a2d0d03c21150
darius <dariusa.bualanjr@pccw.com>
2022-05-20 17:26:24 +0800

    updated monster.js and types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,5 +2,5 @@
  * Override custom element register interface so customElements.define will accept MonsterJS components
  */
 interface CustomElementRegistry {
-    define(name: string, constructor: CustomElementConstructor | ComponentInterface, options?: ElementDefinitionOptions): void;
+    define(name: string, constructor: any, options?: ElementDefinitionOptions): void;
 }
