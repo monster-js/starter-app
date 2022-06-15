332db3d9f7afffe8a618d82fe0c664c2b8b982ea
Lesther Bualan <lestherbualan1718@gmail.com>
2022-06-15 11:55:24 +0800

    use class base style

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -4,53 +4,54 @@ import { Component } from "@monster-js/core";
 @Component('monster-app')
 export class App {
     render() {
-        return <div style="width: 100%;height: 100%;font-family: 'Kanit', sans-serif;text-align: center;">
-            <div style="position: absolute;top: 45%;left: 50%;transform: translate(-50%,-50%);">
-                <div style="text-align: center;margin: 0;padding: 0;">
+        return <div class='container'>
+            <div class='content-wrapper'>
+                <div class='logo'>
                     <img src="../assets/img/dragon.svg" alt="" width='100' height='100'/>
                 </div>
                 <div>
-                    <p style="font-size: 25px;letter-spacing: 3px; color: #143258;margin-top:10px;">Welcome to <strong>MonsterJs</strong>!</p>
+                    <p class='logo-desc'>Welcome to <strong>MonsterJs</strong>!</p>
                 </div>
-                <div>
-                    <div style="display: flex;width: 100%;">
-                        <div style="border-radius: 5px;border: 1px solid rgb(170, 170, 170);padding: 12px 10px;margin: 5px 10px;width: 150px;">
-                            <a style="color: #143258;font-size: 14px;text-decoration: none;" href="https://monster-js.org/docs/" target="_blank"><i style="margin-right: 10px;font-size: 18px;color: #143258;" class="fa-solid fa-rocket"></i>Get Started here</a>
+                <div class='references'>
+                    <div class='group'>
+                        <div class='admonition'>
+                            <a href="https://monster-js.org/docs/" target="_blank"><i class="fa-solid fa-rocket"></i>Get Started here</a>
                         </div>
-                        <div style="border-radius: 5px;border: 1px solid rgb(170, 170, 170);padding: 12px 10px;margin: 5px 10px;width: 150px;">
-                            <a style="color: #143258;font-size: 14px;text-decoration: none;" href="https://monster-js.org/docs/cli-installation" target="_blank"><i style="margin-right: 10px;font-size: 18px;color: #143258;"  class="fa-solid fa-terminal"></i>CLI Guide</a>
+                        <div class="admonition">
+                            <a href="https://monster-js.org/docs/cli-installation" target="_blank"><i class="fa-solid fa-terminal"></i>CLI Guide</a>
                         </div> 
                     </div>
-                    <div style="display: flex;width: 100%; justify-content: center;">
-                        <div style="border-radius: 5px;border: 1px solid rgb(170, 170, 170);padding: 12px 10px;margin: 5px 10px;width: 150px;">
-                            <a style="color: #143258;font-size: 14px;text-decoration: none;" href="https://monster-js.org/docs/testing" target="_blank"><i style="margin-right: 10px;font-size: 18px;color: #143258;"  class="fa-solid fa-code"></i>Testing</a>
+                    <div class='group justify-center'>
+                        <div class="admonition">
+                            <a href="https://monster-js.org/docs/testing" target="_blank"><i class="fa-solid fa-code"></i>Testing</a>
                         </div>
                     </div>
                 </div>
-            </div>   
-            <div style="position: fixed; bottom:0;left:50%; transform:translate(-50%); margin-bottom: 20px;">
-                <p style="font-size: 16px; color: #143258; margin-bottom: 5px;">Available Built-in Packages</p>
-                <div style="display: inline-flex; margin-bottom: 15px;">
-                    <div style="margin: 5px 10px;">
-                        <a style="text-decoration: underline;font-size: 14px;color: #143258;" href="https://monster-js.org/docs/router" target="_blank">Router</a>
+            </div>
+            <div class='footer'>
+                <p class='descriptions'>Available Built-in Packages</p>
+                <div class="group">
+                    <div class='packages'>
+                        <a href="https://monster-js.org/docs/router" target="_blank">Router</a>
                     </div>
-                    <div style="margin: 5px 10px;">
-                        <a style="text-decoration: underline;font-size: 14px;color: #143258;" href="https://monster-js.org/docs/store" target="_blank">Store</a>
+                    <div class='packages'>
+                        <a href="https://monster-js.org/docs/store" target="_blank">Store</a>
                     </div>
-                    <div style="margin: 5px 10px;">
-                        <a style="text-decoration: underline;font-size: 14px;color: #143258;" href="https://monster-js.org/docs/http" target="_blank">HTTP</a>
+                    <div class="packages">
+                        <a href="https://monster-js.org/docs/http" target="_blank">HTTP</a>
                     </div>   
-                </div>  
-                <div style="bottom: 0;align-items: center;width: 100%;">
-                    <div style="text-align: center; padding: 20px;">
-                        <a style="font-size: 16px; padding: 20px 20px;" href="https://www.facebook.com/fbmonsterjs" target="_blank"><i style="color: #143258;" class="fa-brands fa-facebook"></i></a>
-                        <a style="font-size: 16px; padding: 20px 20px;" href="https://twitter.com/mfpjayb" target="_blank"><i style="color: #143258;" class="fa-brands fa-twitter"></i></a>
-                        <a style="font-size: 16px; padding: 20px 20px;" href="https://discord.gg/CY28Qq5yWE" target="_blank"><i style="color: #143258;" class="fa-brands fa-discord"></i></a>
-                        <a style="font-size: 16px; padding: 20px 20px;" href="https://stackoverflow.com/questions/tagged/monster-js" target="_blank"><i style="color: #143258;" class="fa-brands fa-stack-overflow"></i></a>
-                        <a style="font-size: 16px; padding: 20px 20px;" href="https://github.com/monster-js" target="_blank"><i style="color: #143258;" class="fa-brands fa-github"></i></a>
+                </div>                                 
+                <div class='socials-wrapper'>
+                    <div class='wrapper'>
+                         <a href="https://www.facebook.com/fbmonsterjs" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="https://twitter.com/mfpjayb" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="https://discord.gg/CY28Qq5yWE" target="_blank"><i class="fa-brands fa-discord"></i></a>
+                        <a href="https://stackoverflow.com/questions/tagged/monster-js" target="_blank"><i class="fa-brands fa-stack-overflow"></i></a>
+                        <a href="https://github.com/monster-js" target="_blank"><i class="fa-brands fa-github"></i></a>
                     </div>
-                </div>                               
-            </div>         
+                   
+                </div>
+            </div>
         </div>
     }
 }
